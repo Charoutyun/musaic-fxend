d6273632e1f04077c3d3ac15c365822c2b928425
skydwagon <marriamj2002@csu.fullerton.edu>
2024-11-06 14:41:08 -0800

    test

--- app/(components)/OAuthButtons.tsx
+++ app/(components)/OAuthButtons.tsx
@@ -1,11 +1,9 @@
 // components/OAuthButtons.tsx
 import { Button } from "@/components/ui/button";
 import {
-  SiGoogle,
   SiSpotify,
 } from "@icons-pack/react-simple-icons";
 import {
-  signInWithGoogle,
   signInWithSpotify,
 } from "@/app/actions";
 
@@ -28,13 +26,6 @@ const OAuthButton = ({ formAction, children, className }: OAuthButtonProps) => {
 export const OAuthButtons = ({ actionType }: { actionType: "signUp" | "signIn" }) => {
   return (
     <>
-      <OAuthButton
-        formAction={signInWithGoogle}
-        className="relative bg-[#4285F4] hover:bg-[#2e61b3] w-full"
-      >
-        <SiGoogle className="absolute left-2" />
-        Sign {actionType === "signIn" ? "in" : "up"} with Google
-      </OAuthButton>
       <OAuthButton
         formAction={signInWithSpotify}
         className="relative bg-[#1DB954] hover:bg-[#189a46] w-full"
