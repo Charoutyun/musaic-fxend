ca987466c102efe4a15c2e9735becf22c2a1285d
skydwagon <marriamj2002@csu.fullerton.edu>
2024-11-12 14:25:24 -0800

    protected set up

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -17,30 +17,14 @@ export default async function ProtectedPage() {
 
 	//Div elements and UI of /protected/
 	return (
-		<div className="page-container">
-			{/* Left mosaic */}
-			<div className="side-tiles left">
-				{Array.from({ length: 10 }).map((_, i) => (
-					<div key={i} className="tile" style={{ "--i": i }}></div>
-				))}
-			</div>
-
-			{/* Authentication */}
 			<div className="auth-card">
-				<h1 className="auth-head">Welcome, {user.email}!</h1>
+				<h1 className="auth-head">Welcome,</h1>
+				<h1 className="auth-head">{user.email}!</h1>
 				<p className="auth-msg">Your account has been successfully authenticated.</p>
 				<p className="auth-click">
 					Click below to access the music player and chatroom.
 				</p>
 				<EnterButton /> 
 			</div>
-
-			{/* Right mosaic */}
-			<div className="side-tiles right">
-				{Array.from({ length: 10 }).map((_, i) => (
-					<div key={i} className="tile" style={{ "--i": i }}></div>
-				))}
-			</div>
-		</div>
 	);
 }
