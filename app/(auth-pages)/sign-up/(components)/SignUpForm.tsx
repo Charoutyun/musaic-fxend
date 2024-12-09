9061d43fbf9fda95fe4e081e2228c276050b39ee
skydwagon <marriamj2002@csu.fullerton.edu>
2024-12-09 12:43:00 -0800

    fixed alignment

--- app/(auth-pages)/sign-up/(components)/SignUpForm.tsx
+++ app/(auth-pages)/sign-up/(components)/SignUpForm.tsx
@@ -12,19 +12,19 @@ export const SignUpForm = ({ searchParams }: { searchParams: Message }) => {
 				<form className="flex-1 flex flex-col min-w-64 space-y-2.5">
 					<h1 className="text-center">Sign up</h1>
 					<div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-					<div className="flex flex-col gap-2 mt-8">
-						<p className="text-center">
-						To sign up, please fill out the form below:
-						</p>
-						<a
-						href="https://docs.google.com/forms/d/e/1FAIpQLSc1iAtvR8LclSbexHsulPTdOkFvq5dBZc7BDBH4xly3BKePTw/viewform?usp=header"
-						target="_blank"
-						rel="noopener noreferrer"
-						className="text-center bg-primary text-black py-2 px-4 rounded hover:bg-primary-dark"
-						>
-						Go to Sign-Up Form
-						</a>
-					</div>
+						<div className="flex flex-col gap-2 mt-8">
+							<p className="text-center">
+							To sign up, please fill out the form below:
+							</p>
+							<a
+								href="https://docs.google.com/forms/d/e/1FAIpQLSc1iAtvR8LclSbexHsulPTdOkFvq5dBZc7BDBH4xly3BKePTw/viewform?usp=header"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="text-center bg-primary text-black py-2 px-4 rounded hover:bg-primary-dark"
+							>
+							Go to Sign-Up Form
+							</a>
+						</div>
 						<small>
 							Already have an account?{" "}
 							<Link
